9aab90b09ff23b0cdd12ceec0da0d4b5bb387a5a
284445233 <103988525+284445233@users.noreply.github.com>
2025-11-15 13:53:37 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -109,8 +109,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["8.134.194.45"];
+pub const RS_PUB_KEY: &str = "liubo284445233";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
